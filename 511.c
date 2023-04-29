f007a795c441af34c0c2e5f9e6ca74e2b4637ba7
RaKsHiT121 <132118995+RaKsHiT121@users.noreply.github.com>
2023-04-29 10:05:26 +0530

    Add files via upload

---
+++ 511.c
@@ -0,0 +1,23 @@
+// to check whether a no. is divisble by 5 and 11 or not
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("enter no: ");
+    scanf("%d",&i);
+    if (i%5==0)
+    {
+        if (i%11==0)
+        printf("%d is divisible by both 5 &11",i);
+        else
+        printf("%d is divisible by 5",i);
+    }
+    else
+    {
+    if (i%11==0)
+    printf("%d is divisible by 11",i);
+    }
+    if (i%5!=0&&i%11!=0)
+    printf("%d is divisible neither by 5 nor by 11",i);
+}
